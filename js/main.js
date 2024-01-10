1d69c0c88ba807543861f77880535f65e784b423
BISTRIYKURIER <88018934+BISTRIYKURIER@users.noreply.github.com>
2024-01-10 17:21:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+window.onload =function(){const termal1=document.getElementsByTagName("button")[5];
+const termal2=document.getElementsByTagName("button")[6];
+const termal3=document.getElementsByTagName("button")[7];
+const termal4=document.getElementsByTagName("button")[8];
+const termal5=document.getElementsByTagName("button")[9];
+const termal6=document.getElementsByTagName("button")[10];
+const termal7=document.getElementsByTagName("button")[11];
+const termal8=document.getElementsByTagName("button")[12];
+
+termal1.onmouseover = function() { 
+termal1.style.backgroundImage= "url(img/tr1.png)";};
+termal1.onmouseleave = function() { 
+termal1.style.backgroundImage="url(img/1.png)";};
+
+termal2.onmouseover = function() { 
+termal2.style.backgroundImage= "url(img/tr2.png)";};
+termal2.onmouseleave = function() { 
+termal2.style.backgroundImage="url(img/2.png)";};
+
+termal3.onmouseover = function() { 
+termal3.style.backgroundImage= "url(img/tr3.png)";};
+termal3.onmouseleave = function() { 
+termal3.style.backgroundImage="url(img/3.png)";};
+
+termal4.onmouseover = function() { 
+termal4.style.backgroundImage= "url(img/tr4.png)";};
+termal4.onmouseleave = function() { 
+termal4.style.backgroundImage="url(img/4.png)";};
+
+termal5.onmouseover = function() { 
+termal5.style.backgroundImage= "url(img/tr5.png)";};
+termal5.onmouseleave = function() { 
+termal5.style.backgroundImage="url(img/5.png)";};
+
+termal6.onmouseover = function() { 
+termal6.style.backgroundImage= "url(img/tr6.png)";};
+termal6.onmouseleave = function() { 
+termal6.style.backgroundImage="url(img/6.png)";};
+
+termal7.onmouseover = function() { 
+termal7.style.backgroundImage= "url(img/tr7.png)";};
+termal7.onmouseleave = function() { 
+termal7.style.backgroundImage="url(img/7.png)";};
+
+termal8.onmouseover = function() { 
+termal8.style.backgroundImage= "url(img/tr8.png)";};
+termal8.onmouseleave = function() { 
+termal8.style.backgroundImage="url(img/8.png)";};
+}
+
+
+
+  
